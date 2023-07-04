b43c5274f1647d53867a28c56e01979ae621cd40
programmerbits <programmerbits@gmail.com>
2023-07-04 12:57:21 -0400

    Add helper for SPA proxy using YARP

---
+++ src/Rig.Api/YarpExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using System.Net.Mime;
+using Yarp.ReverseProxy.Forwarder;
+using Microsoft.AspNetCore.Http;
+
+namespace Rig.Api;
+
+public static class YarpExtensions
+{
+    public static void MapSpaProxy(this WebApplication app, string spaServer)
+    {
+        var forwarder = app.Services.GetRequiredService<IHttpForwarder>();
+
+        var httpClient = new HttpMessageInvoker(new SocketsHttpHandler()
+        {
+            UseProxy = false,
+            AllowAutoRedirect = false,
+            AutomaticDecompression = DecompressionMethods.None,
+            UseCookies = false
+        });
+
+        app.Map("{**catch-all}", async httpContext =>
+        {
+            if (!httpContext.User.Identity?.IsAuthenticated ?? false)
+            {
+                await httpContext.ChallengeAsync();
+                return;
+            }
+
+            await forwarder.SendAsync(httpContext, spaServer, httpClient);
+
+            if (httpContext.Response.ContentType == MediaTypeNames.Text.Html)
+            {
+                // do not cache the HTML page coming back from the SPA
+                httpContext.Response.Headers.Append("Cache-Control", "no-cache");
+            }
+        });
+    }
+}
